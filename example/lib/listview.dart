f358f4b16a5515212a0b7ff816a119f2e04b4cf5
befovy <befovy@gmail.com>
2019-12-20 09:27:49 +0800

    feat: new listView , wip

--- example/lib/listview.dart
+++ example/lib/listview.dart
@@ -1,9 +1,54 @@
 import 'package:fijkplayer/fijkplayer.dart';
-import 'package:fijkplayer_example/my_sliver_child_builder_delegate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'app_bar.dart';
+
+class ListChildBuilderDelegate extends SliverChildBuilderDelegate {
+  ListChildBuilderDelegate(builder)
+      : super(
+          builder,
+        );
+
+  @override
+  void didFinishLayout(int firstIndex, int lastIndex) {
+    // print("first $firstIndex, last $lastIndex");
+  }
+}
+
+class ListItemPlayer extends StatefulWidget {
+  final int index;
+
+  ListItemPlayer({@required this.index});
+
+  @override
+  _ListItemPlayerState createState() => _ListItemPlayerState();
+}
+
+class _ListItemPlayerState extends State<ListItemPlayer> {
+  @override
+  void initState() {
+    super.initState();
+    FijkLog.d("list initState ${widget.index}", tag: "list");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    FijkLog.d("list deactivate ${widget.index}", tag: "list");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    FijkLog.d("list dispose ${widget.index}", tag: "list");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 260, child: Image.asset('assets/cover.png'));
+  }
+}
 
 class ListScreen extends StatefulWidget {
   @override
@@ -19,52 +64,26 @@ class _ListScreenState extends State<ListScreen> {
     super.initState();
   }
 
+  Widget listBuilder(BuildContext context, int index) {
+    return ListItemPlayer(index: index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: FijkAppBar.defaultSetting(title: "List View"),
-      body: ListView.custom(
-        childrenDelegate: MySliverChildBuilderDelegate(
-          (BuildContext context, int index) {
-            if (!_players.containsKey(index)) {
-              _players[index] = FijkPlayer();
-              _players[index].setDataSource("asset:///assets/butterfly.mp4");
-            }
-            FijkLog.i("build list item $index", tag: "list");
-            return Column(
-              children: <Widget>[
-                Container(
-                  height: 240.0,
-                  child: FijkView(
-                    player: _players[index],
-                  ),
-                ),
-                SizedBox(height: 12.0)
-              ],
-            );
+        appBar: FijkAppBar.defaultSetting(title: "List View"),
+        body: NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification notification) {
+            //print("notification ${notification.metrics.pixels}");
+            return false;
           },
-          childCount: listItemCount,
-          onItemVisibilityState: setVideoVisibilityState,
-        ),
-        //设置cacheExtent 为0.0 不然获取到的显示隐藏的index不准确
-        cacheExtent: 0.0,
-      ),
-    );
+          child: ListView.custom(
+            childrenDelegate: ListChildBuilderDelegate(listBuilder),
+            cacheExtent: 0.0,
+          ),
+        ));
   }
 
-  void setVideoVisibilityState(List<int> exposure, List<int> hidden) {
-    exposure.forEach((index) {
-      //显示的indexs
-      if (_players[index]?.state == FijkState.idle) {
-        _players[index]?.setDataSource("asset:///assets/butterfly.mp4");
-      }
-    });
-    hidden.forEach((index) async {
-      //隐藏的indexs
-       _players[index]?.stop();
-       _players[index]?.reset();
-    });
-  }
 
   @override
   void dispose() {
